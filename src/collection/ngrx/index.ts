b480ea91452b8070d241e4eaeffad4340ee84658
Patryk Zieliński <patryk.zielinski@valueadd.pl>
2019-04-26 10:55:54 +0200

    feat(ngrx): add blank ngrx schematic

--- src/collection/ngrx/index.ts
+++ src/collection/ngrx/index.ts
@@ -1,9 +1,11 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NgrxSchema } from './ngrx-schema.interface';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function vaSchematics(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+export function ngrx(options: NgrxSchema): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.info(JSON.stringify(options));
     return tree;
   };
 }
