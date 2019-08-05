d70c9f9459c5ac7ffaa6329ea0b7a060a1efa383
Patryk Zieliński <patryk.zielinski93@gmail.com>
2019-08-05 11:37:02 +0200

    replace @valueadd/common with @valueadd/testing

--- src/collection/crud/rules/crud-effects.rule.ts
+++ src/collection/crud/rules/crud-effects.rule.ts
@@ -291,7 +291,7 @@ export function crudEffects(options: CrudOptions): Rule {
     insertTypeImport(host, stateDir.effectsSpec, `DataPersistence`);
     insertCustomImport(host, stateDir.effectsSpec, 'hot', 'jasmine-marbles');
     insertCustomImport(host, stateDir.effectsSpec, 'cold', 'jasmine-marbles');
-    insertCustomImport(host, stateDir.effectsSpec, 'getClassMethodsNames', '@valueadd/common');
+    insertCustomImport(host, stateDir.effectsSpec, 'getClassMethodsNames', '@valueadd/testing');
 
     insert(host, stateDir.effects, createEffects(host, options));
     insert(host, stateDir.effectsSpec, createEffectsSpec(host, options));
